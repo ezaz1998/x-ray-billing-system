8783298a05044291f177a6766f79450f941996c8
Ezaz Ahmed <aezaz203@gmail.com>
2026-03-11 22:21:16 +0530

    fix render port

--- server.js
+++ server.js
@@ -344,6 +344,8 @@ res.status(500).send("Delete failed");
 
 });
 
-app.listen(3000, () => {
-  console.log("Server running on port 3000");
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log("Server running on port " + PORT);
 });
